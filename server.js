1cbd1607a6d3c05b840b4fe518d1f9e155b67276
gwynncausing <gwynn.causing23@gmail.com>
2021-10-18 18:04:04 +0800

    change response

--- server.js
+++ server.js
@@ -24,6 +24,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("it is working");
+});
 app.post("/signin", (req, res) => {
   signin.handleSignin(req, res, db, bcrypt);
 });
@@ -44,6 +47,6 @@ app.post("/imageurl", (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen(process.env.PORT || 3000), () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("app is running on port " + process.env.PORT);
 });
